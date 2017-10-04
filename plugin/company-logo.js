a4e000a1863131f5f32e688f6e4964bc32996d91
Jeff Doktor <jdoktor@brightcove.com>
2017-10-04 14:26:15 -0600

    update code

--- plugin/company-logo.js
+++ plugin/company-logo.js
@@ -5,28 +5,28 @@ videojs.registerPlugin('companyLogo', function() {
       newLink = document.createElement('a'),
 	    newImage = document.createElement('img');
 
-	// Assign id and class to div for logo icon
-	newElement.id = 'companyLogo';
-	newElement.className = 'vjs-control';
+  // Assign id and class to div for logo icon
+  newElement.id = 'companyLogo';
+  newElement.className = 'vjs-control';
 
   // Uncomment next line to see the newly created company logo div element
-	//console.log("button: ",newElement);
+  //console.log("button: ",newElement);
 
-	// Assign property to div element
-	newImage.setAttribute('src','http://solutions.brightcove.com/bcls/assets/images/bc-logo-white.png');
-	// Assign properties to a element
-	newLink.setAttribute('href','http://www.brightcove.com');
-	// Uncomment next line to open link in a new browser tab
-	//newLink.setAttribute('target', '_blank');
-	// Add img element to parent a element
-	newLink.appendChild(newImage);
-	// Add a element to parent div element
-	newElement.appendChild(newLink);
+  // Assign property to div element
+  newImage.setAttribute('src','http://solutions.brightcove.com/bcls/assets/images/bc-logo-white.png');
+  // Assign properties to a element
+  newLink.setAttribute('href','http://www.brightcove.com');
+  // Uncomment next line to open link in a new browser tab
+  //newLink.setAttribute('target', '_blank');
+  // Add img element to parent a element
+  newLink.appendChild(newImage);
+  // Add a element to parent div element
+  newElement.appendChild(newLink);
 
-	// Get control bar
+  // Get control bar
   // Remember that getElementsByClassName() returns an array
   controlBar = document.getElementsByClassName('vjs-control-bar')[0];
 
-	// Insert the logo icon to the end of the control bar elements
-	controlBar.appendChild(newElement);
+  // Insert the logo icon to the end of the control bar elements
+  controlBar.appendChild(newElement);
 });
